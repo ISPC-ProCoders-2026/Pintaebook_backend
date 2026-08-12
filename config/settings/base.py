411716663dae001027b0d4fa58448731b989912b
Francisco <torofrancisco13@gmail.com>
2026-08-12 00:10:27 -0300

    feat(users): user models added using AbstractUser way

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'apps.accounts',
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
